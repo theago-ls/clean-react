c131722ccf9a86ae550de0ae52d33a268d85208e
Thiago Lourenço <thiagosup1@gmail.com>
2020-11-23 22:15:30 -0400

    fix: typo coverageDirectory jest config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     collectCoverageFrom: [
         '<rootDir>/src/**/*.{ts,tsx}'
     ],
-    covergeDirectory: 'coverage',
+    coverageDirectory: 'coverage',
     testEnvironment: 'node',
     transform: {
         '.+\\.ts$':'ts-jest'
