7098208de54aeec470c8ac9f67d271245b5cf702
Thiago Lourenço <thiagosup1@gmail.com>
2020-11-26 19:59:11 -0400

    refactor: add paths alias in tsconfig

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,5 @@
-import { mockAuthenticaction } from './../../../domain/test/mock-authentication'
-import { HttpPostClientSpy } from '../../test/mock-http-client'
+import { mockAuthenticaction } from '@/domain/test/mock-authentication'
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 import faker from 'faker'
 
