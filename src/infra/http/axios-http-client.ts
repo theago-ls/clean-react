13d4251142db41e306843684cd28acb9e5c0c64c
Thiago Lourenço <thiagosup1@gmail.com>
2020-11-28 21:57:47 -0400

    refactor: group imports

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -1,5 +1,4 @@
-import { HttpResponse } from '@/data/protocols/http'
-import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
+import { HttpResponse, HttpPostClient, HttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
