907add48cf77a617f5015699e71eb568ae4ee923
Thiago Lourenço <thiagosup1@gmail.com>
2020-11-29 22:55:38 -0400

    feat: ensure Login will not load spinner and error on start

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Styles from './login-styles.scss'
 import { Footer, LoginHeader, FormStatus, Input, SubmitButton } from '@/presentation/components'
 
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
+
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className={Styles.loginWrap}>
       <LoginHeader />
       <form data-testid="form" className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" state={{}} setState={() => {}} />
-        <Input type="password" name="password" placeholder="Digite sua senha" state={{}} setState={() => {}} />
-        <SubmitButton state={{}} text="Entrar"/>
-        <FormStatus state={{}} />
+        <Input type="email" name="email" placeholder="Digite seu e-mail" state={state} setState={() => {}} />
+        <Input type="password" name="password" placeholder="Digite sua senha" state={state} setState={() => {}} />
+        <SubmitButton state={state} text="Entrar"/>
+        <FormStatus state={state} />
       </form>
       <Footer />
     </div>
