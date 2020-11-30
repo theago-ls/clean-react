f0d70f6746f0414cc6558a8e3efd45dd44f1cbcb
Thiago Lourenço <thiagosup1@gmail.com>
2020-11-30 19:55:55 -0400

    feat: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,24 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Styles from './login-styles.scss'
 import { Footer, LoginHeader, FormStatus, Input, SubmitButton } from '@/presentation/components'
+import { Validation } from '@/presentation/protocols/validation/validation'
 
-const Login: React.FC = () => {
-  const [state] = useState({
+type Props = {
+  validation: Validation
+}
+
+const Login: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
     isLoading: false,
     errorMessage: '',
     isFormInvalid: true,
+    email: '',
     emailError: 'Campo obrigatório',
-    passwordError: 'Campo obrigatório'
+    passwordError: 'Campo obrigatório',
+    mainError: ''
   })
+
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  useEffect(() => {
+    validation.validate({ email: state.email })
+  }, [state.email])
 
   return (
     <div className={Styles.loginWrap}>
       <LoginHeader />
       <form data-testid="form" className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" state={state} setState={() => {}} />
-        <Input type="password" name="password" placeholder="Digite sua senha" state={state} setState={() => {}} />
+        <Input type="email" name="email" placeholder="Digite seu e-mail" state={state} setState={setState} onChange={handleChange}/>
+        <Input type="password" name="password" placeholder="Digite sua senha" state={state} setState={setState} />
         <SubmitButton state={state} text="Entrar"/>
         <FormStatus state={state} />
       </form>
