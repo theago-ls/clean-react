13a6fa5fd1c19ea88b563d1efe0bb68d894e048c
Thiago Lourenço <thiagosup1@gmail.com>
2020-11-30 19:56:21 -0400

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,25 +1,47 @@
 import React from 'react'
-import { render, RenderResult, screen, waitFor } from '@testing-library/react'
+import { render, RenderResult, fireEvent, waitFor } from '@testing-library/react'
 import { Login } from '@/presentation/pages'
 import '@testing-library/jest-dom/extend-expect'
+import { Validation } from '@/presentation/protocols/validation/validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Login page', () => {
   test('Should start with initial state', async () => {
-    const { sut } = makeSut()
+    const { sut, validationSpy } = makeSut()
     expect(sut.queryByTestId('spinner')).not.toBeInTheDocument()
     expect(sut.getByTestId('submit')).toBeDisabled()
     expect(sut.getByTestId('email').title).toBe('Campo obrigatório')
     expect(sut.getByTestId('password').title).toBe('Campo obrigatório')
+  })
+
+  test('Should call Validation with correct value', async () => {
+    const { sut, validationSpy } = makeSut()
+    fireEvent.input(sut.getByTestId('email'), { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
