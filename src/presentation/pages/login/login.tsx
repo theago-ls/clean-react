834cc4c7762ed2e090fca5f01d544773b0fd8569
Thiago Lourenço <thiagosup1@gmail.com>
2020-11-30 21:40:03 -0400

    feat: ensure Login show Authentication errors

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -31,14 +31,18 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
-    if (state.isLoading || state.emailError || state.passwordError) {
-      return
+    try {
+      if (state.isLoading || state.emailError || state.passwordError) {
+        return
+      }
+      setState(prevState => ({ ...prevState, isLoading: true }))
+      await authentication.auth({
+        email: state.email,
+        password: state.password
+      })
+    } catch (error) {
+      setState(prevState => ({ ...prevState, isLoading: false, mainError: error.message }))
     }
-    setState(prevState => ({ ...prevState, isLoading: true }))
-    await authentication.auth({
-      email: state.email,
-      password: state.password
-    })
   }
 
   useEffect(() => {
