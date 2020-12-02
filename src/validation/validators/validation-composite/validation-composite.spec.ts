84f3810ca28f2e311930d46ce4253bc955a806aa
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-01 21:01:27 -0400

    feat: ensure ValidationComposite returns the first error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,12 +4,13 @@ import { ValidationComposite } from './validation-composite'
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_message')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_message')
+    fieldValidationSpy2.error = new Error('second_message')
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
-    expect(sut.validate('any_field', 'any_value')).toBe('any_message')
+    expect(sut.validate('any_field', 'any_value')).toBe('first_message')
   })
 })
