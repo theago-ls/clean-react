b2c32912a6e9036720ab67ae5dda79dbd60aab2e
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-04 20:38:25 -0400

    feat: ensure Signup change name status to error if Validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,31 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Footer, FormStatus, Input, LoginHeader, SubmitButton } from '@/presentation/components'
 import Styles from './signup-styles.scss'
+import { Validation } from '@/presentation/protocols'
 
-const Signup: React.FC = () => {
-  const [state] = useState({
+type Props = {
+  validation: Validation
+}
+
+const Signup: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
     isLoading: false,
     name: '',
     email: '',
     password: '',
     passwordConfirmation: '',
-    nameError: 'Campo obrigatório',
+    nameError: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     passwordConfirmationError: 'Campo obrigatório',
     mainError: ''
   })
 
+  useEffect(() => {
+    setState(prevState => ({
+      ...prevState,
+      nameError: validation.validate('email', state.name)
+    }))
+  }, [state.name])
+
   return (
     <div className={Styles.signup}>
       <LoginHeader />
       <form className={Styles.form} onSubmit={() => {}}>
         <h2>Criar conta</h2>
-        <Input data-testid="name" type="text" name="name" placeholder="Digite seu nome" state={state} setState={() => {}} onChange={() => {}}/>
-        <Input data-testid="email" type="email" name="email" placeholder="Digite seu e-mail" state={state} setState={() => {}} onChange={() => {}}/>
-        <Input data-testid="password" type="password" name="password" placeholder="Digite sua senha" state={state} setState={() => {}} />
-        <Input data-testid="passwordConfirmation" type="password" name="passwordConfirmation" placeholder="Confirme a sua senha" state={state} setState={() => {}} />
+        <Input data-testid="name" type="text" name="name" placeholder="Digite seu nome" state={state} setState={setState} onChange={() => {}}/>
+        <Input data-testid="email" type="email" name="email" placeholder="Digite seu e-mail" state={state} setState={setState} onChange={() => {}}/>
+        <Input data-testid="password" type="password" name="password" placeholder="Digite sua senha" state={state} setState={setState} />
+        <Input data-testid="passwordConfirmation" type="password" name="passwordConfirmation" placeholder="Confirme a sua senha" state={state} setState={setState} />
         <SubmitButton data-testid="submit" state={state} text="Entrar"/>
         <span className={Styles.link}>Logar em uma conta existente</span>
         <FormStatus state={state} />
