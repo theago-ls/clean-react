b29fce22a83cceb0b55987e548891991e7e99e19
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-04 21:41:44 -0400

    test: ensure Signup does not call AddAccount if Validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -122,4 +122,11 @@ describe('SignUp', () => {
     simulateValidSubmit(sut)
     expect(addAccountSpy.callsCount).toBe(1)
   })
+
+  test('Should not call AddAccount if form is invalid', async () => {
+    const validationError = faker.random.words()
+    const { sut, addAccountSpy } = makeSut({ validationError })
+    await simulateValidSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(0)
+  })
 })
