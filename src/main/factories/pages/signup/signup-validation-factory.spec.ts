0d2cf0136607479cd1f70c67ae60197263acf93d
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-07 20:29:43 -0400

    test: add Signup Validation Factory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,13 @@
+import { makeSignUpValidation } from './signup-validation-factory'
+import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
+
+describe('SignupValidationFactory', () => {
+  test('should compose ValidationComposite with correct validations', () => {
+    expect(makeSignUpValidation()).toEqual(ValidationComposite.build([
+      ...ValidationBuilder.field('name').required().min(5).build(),
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().min(5).build(),
+      ...ValidationBuilder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
