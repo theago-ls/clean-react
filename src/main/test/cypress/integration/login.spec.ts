601e8c41c6ff9b1d1d0d856c4a17a10a871fad4a
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-09 22:03:26 -0400

    test: ensure Login present valid state if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -13,10 +13,17 @@ describe('Login', () => {
 
   it('should show error state if form is invalid', () => {
     cy.getByTestId('email').focus().type(faker.random.word())
-    cy.getByTestId('email').should('have.attr', 'title', 'Valor inválido')
+    cy.getByTestId('email').should('have.attr', 'title', 'E-mail inválido.')
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
-    cy.getByTestId('password').should('have.attr', 'title', 'Valor inválido')
+    cy.getByTestId('password').should('have.attr', 'title', 'Campo com número de caracteres abaixo do mínimo requerido.')
     cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('should show valid state if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
