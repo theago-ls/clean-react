e99d5d2c7cfcb6f9db331574b6e56497cb1a2aa7
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-11 21:39:27 -0400

    test: ensure Login shows UnexpectedError on 400

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -29,7 +29,17 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
-  it('should show error if invalid credentials are provided', () => {
+  it('should show InvalidCredentialsError if invalid credentials are provided', () => {
+    cy.intercept(
+      '/login',
+      (req) => {
+        req.reply(401, {
+          error: {
+            message: faker.random.words()
+          }
+        })
+      }
+    )
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
@@ -40,13 +50,38 @@ describe('Login', () => {
   })
 
   it('should save accessToken if valid credentials are provided', () => {
+    cy.intercept(
+      '/login',
+      (req) => {
+        req.reply(200, {
+          accessToken: faker.random.uuid()
+        })
+      }
+    )
     cy.getByTestId('email').focus().type('mango@gmail.com')
     cy.getByTestId('password').focus().type('12345')
     cy.getByTestId('submit').click()
-    cy.getByTestId('error-wrap')
-      .getByTestId('spinner').should('not.exist')
-      .getByTestId('main-error').should('not.exist')
     cy.url().should('eq', `${baseUrl}/`)
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+  })
+
+  it('should show UnexpectedError on 400', () => {
+    cy.intercept(
+      '/login',
+      (req) => {
+        req.reply(400, {
+          error: {
+            message: faker.random.words()
+          }
+        })
+      }
+    )
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('not.exist')
+      .getByTestId('main-error').should('contains.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+    cy.url().should('eq', `${baseUrl}/login`)
   })
 })
