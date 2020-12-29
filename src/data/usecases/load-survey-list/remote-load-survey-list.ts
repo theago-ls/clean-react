c8dfa1e5c84ceb429606d28f4a2fedccf819bbe3
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-28 21:40:33 -0400

    feat: ensure RemoteLoadSurveyList returns an empty list on 204

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -15,6 +15,7 @@ export class RemoteLoadSurveryList implements LoadSurveyList {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: return httpResponse.body
+      case HttpStatusCode.noContent: return []
       default: throw new UnexpectedError()
     }
   }
