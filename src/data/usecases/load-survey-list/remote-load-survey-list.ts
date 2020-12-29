54ee88d0ac08e10aefc5c5618fe3fad94bb844f1
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-28 21:18:25 -0400

    feat: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,4 +1,5 @@
-import { HttpGetClient } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/errors'
+import { HttpStatusCode, HttpGetClient } from '@/data/protocols/http'
 
 export class RemoteLoadSurveryList {
   constructor (
@@ -9,6 +10,10 @@ export class RemoteLoadSurveryList {
   }
 
   async loadAll (): Promise<void > {
-    await this.httpGetClient.get({ url: this.url })
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      default: throw new UnexpectedError()
+    }
   }
 }
