416812fc395781993cf08f5fa037aeeb3fea4e88
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-30 19:14:07 -0400

    test: ensure CurrentAccountAdapter throws if an invalid account is provided

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -3,6 +3,6 @@ import { UnexpectedError } from '@/domain/errors'
 import { makeLocalStorageAdapter } from '@/main/factories/cache/local-storage-adapter-factory'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account.accessToken) { throw new UnexpectedError() }
+  if (!account?.accessToken) { throw new UnexpectedError() }
   makeLocalStorageAdapter().set('account', account)
 }
