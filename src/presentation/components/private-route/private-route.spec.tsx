ee54614d746a54788ba0846d2aeb864b8dd3804d
Thiago Lourenço <thiagosup1@gmail.com>
2020-12-30 19:49:50 -0400

    refactor: move SUT creation to a method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from './private-route'
+
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  render(<Router history={history}><PrivateRoute /></Router>)
+
+  return { history }
+}
 
 describe('PrivateRoute', () => {
   test('should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    render(<Router history={history}><PrivateRoute /></Router>)
-
+    const { history } = makeSut()
     expect(history.location.pathname).toBe('/login')
   })
 })
