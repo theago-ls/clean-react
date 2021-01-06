62ae45bea019ab13eec6cc96e48ed764d2e81cf9
Thiago Lourenço <thiagosup1@gmail.com>
2021-01-05 20:01:38 -0400

    refactor: add useLogout hook

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -2,17 +2,16 @@ import React, { memo, useContext } from 'react'
 import { Logo } from '@/presentation/components'
 import Styles from './header-styles.scss'
 import { ApiContext } from '@/presentation/contexts'
-import { useHistory } from 'react-router-dom'
+import { useLogout } from '@/presentation/hooks'
 
 const Header: React.FC = () => {
-  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
+  const { getCurrentAccount } = useContext(ApiContext)
 
-  const history = useHistory()
+  const logout = useLogout()
 
-  const logout = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+  const handleLogout = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
     e.preventDefault()
-    setCurrentAccount(undefined)
-    history.replace('/login')
+    logout()
   }
 
   return (
@@ -21,7 +20,7 @@ const Header: React.FC = () => {
         <Logo />
         <div className={Styles.logoutWrap}>
           <span data-testid="username">{getCurrentAccount().name}</span>
-          <a data-testid="logout" href="#" onClick={logout}>Sair</a>
+          <a data-testid="logout" href="#" onClick={handleLogout}>Sair</a>
         </div>
       </div>
     </header>
--- src/presentation/hooks/use-error-handler.ts
+++ src/presentation/hooks/use-error-handler.ts
@@ -1,19 +1,15 @@
-import { useContext } from 'react'
-import { useHistory } from 'react-router-dom'
 import { AccessDeniedError } from '@/domain/errors'
-import { ApiContext } from '@/presentation/contexts'
+import { useLogout } from '@/presentation/hooks'
 
 type CallbackType = (error: Error) => void
 type ResultType = CallbackType
 
 export const useErrorHandler = (callback: CallbackType): ResultType => {
-  const history = useHistory()
-  const { setCurrentAccount } = useContext(ApiContext)
+  const logout = useLogout()
 
   return (err: Error): void => {
     if (err instanceof AccessDeniedError) {
-      setCurrentAccount(undefined)
-      history.replace('/login')
+      logout()
     } else {
       callback(err)
     }
