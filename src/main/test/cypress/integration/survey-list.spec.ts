1622a90c59deee5cfbbeaecb86a958830599fc70
Thiago Lourenço <thiagosup1@gmail.com>
2021-01-05 20:53:34 -0400

    test: ensure SurveyList logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -1,5 +1,5 @@
-import { mockUnexpectedError } from './../support/survey-list-mocks'
-import { setLocalStorageItem } from './../support/helpers'
+import { mockUnexpectedError, mockAccessDeniedError } from './../support/survey-list-mocks'
+import { setLocalStorageItem, testUrl } from './../support/helpers'
 import faker from 'faker'
 
 describe('SurveyList', () => {
@@ -13,5 +13,11 @@ describe('SurveyList', () => {
     cy.visit('')
     mockUnexpectedError()
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+  })
+
+  it('should logout on AccessDeniedError', () => {
+    cy.visit('')
+    mockAccessDeniedError()
+    testUrl('/login')
   })
 })
--- src/main/test/cypress/support/survey-list-mocks.ts
+++ src/main/test/cypress/support/survey-list-mocks.ts
@@ -1,3 +1,4 @@
 import * as Http from './http-mock'
 
 export const mockUnexpectedError = (): void => Http.mockServerError('/surveys')
+export const mockAccessDeniedError = (): void => Http.mockForbiddenError('/surveys')
