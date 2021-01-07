6e11205316d3399be4fc1aef35f4e8de0d441d1f
Thiago Lourenço <thiagosup1@gmail.com>
2021-01-06 21:26:30 -0400

    chore: config travis ci to use cypress

--- webpack.dev.js
+++ webpack.dev.js
@@ -32,7 +32,8 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './public',
     writeToDisk: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    port: 3000
   },
   plugins: [
     new DefinePlugin({
