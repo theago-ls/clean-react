b881b0354b26ce50f20e11b12c9d5e919c61b6a2
Thiago Lourenço <thiagosup1@gmail.com>
2021-01-08 11:38:58 -0400

    test: ensure SurveyResult redirects to Login on AccessDeniedError

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -4,6 +4,8 @@ import * as Http from '../utils/http-mock'
 // If api path isn't added, Cypress intercepts requests to load page
 const path = 'http://fordevs.herokuapp.com/api/surveys'
 const mockUnexpectedError = (): void => Http.mockServerError(path)
+const mockAccessDeniedError = (): void => Http.mockForbiddenError(path)
+const mockSuccess = (): void => Http.mockOk(path, 'survey-result')
 
 describe('SurveyResult', () => {
   beforeEach(() => {
@@ -13,5 +15,21 @@ describe('SurveyResult', () => {
     mockUnexpectedError()
     cy.visit('/surveys/any_id')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+  })
+
+  // @TODO: https://github.com/cypress-io/cypress/issues/9302
+  // it('should reload on reload button click', () => {
+  //   mockUnexpectedError()
+  //   cy.visit('/surveys/any_id')
+  //   cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+  //   mockSuccess()
+  //   cy.getByTestId('reload').click()
+  //   cy.get('question').should(exist')
+  // })
+
+  it('should redirect to Login on AccessDeniedError', () => {
+    mockAccessDeniedError()
+    cy.visit('/surveys/any_id')
+    testUrl('/login')
   })
 })
