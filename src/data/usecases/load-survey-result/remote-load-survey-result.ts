1a7e7407855613b4264f2809042900e246599220
Thiago Lourenço <thiagosup1@gmail.com>
2021-01-07 20:00:37 -0400

    feat: ensure RemoteLoadSurveyResult returns SurveyResultModel on 200d

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,18 +1,33 @@
+import { LoadSurveyResult } from '@/domain/usecases/load-survey-result'
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { HttpGetClient } from '@/data/protocols/http/http-get-client'
 import { HttpStatusCode } from '@/data/protocols/http'
-export class RemoteLoadSurveyResult {
+export class RemoteLoadSurveyResult implements LoadSurveyResult {
   constructor (
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<RemoteLoadSurveyResult.Model>
   ) {}
 
-  async load (): Promise<void> {
+  async load (): Promise<LoadSurveyResult.Model> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse?.body ? { ...httpResponse.body, date: new Date(httpResponse.body.date) } : null
       case HttpStatusCode.forbidden: throw new AccessDeniedError()
       default: throw new UnexpectedError()
     }
+  }
+}
+
+export namespace RemoteLoadSurveyResult {
+  export type Model = {
+    question: string
+    date: string
+    answers: Array<
+    {
+      image?: string
+      answer: string
+      count: number
+      percent: number
+    }>
   }
 }
--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -6,13 +6,13 @@ export namespace LoadSurveyResult {
   export type Model = {
     question: string
     date: Date
-    answers: [
-      {
-        image?: string
-        answer: string
-        count: number
-        percent: number
-      }
-    ]
+    answers: Array<
+    {
+      image?: string
+      answer: string
+      count: number
+      percent: number
+    }
+    >
   }
 }
